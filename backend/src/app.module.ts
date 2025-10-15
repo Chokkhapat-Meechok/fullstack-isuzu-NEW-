cc19908e7b7a892e6d1e79344023c952e7f0c362
Chokkhapat-Meechok <6731503006@lamduan.mfu.ac.th>
2025-10-15 13:34:03 +0000

    แก้ไฟล์ app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', // ถ้าใช้ XAMPP หรือ WAMP ก็ไม่ต้องแก้
+      port: 3306,
+      username: 'root',
+      password: '', // ถ้ามีรหัสผ่านใน MySQL ให้ใส่ตรงนี้
+      database: 'fullstack_db',
+      autoLoadEntities: true,
+      synchronize: true, // ให้มันสร้างตารางให้อัตโนมัติ
+    }),
+  ],
 })
 export class AppModule {}
