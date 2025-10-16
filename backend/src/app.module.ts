c546875b2e65d80b96c34b407bb6f8267a4c71a5
Chokkhapat-Meechok <6731503006@lamduan.mfu.ac.th>
2025-10-16 13:23:10 +0000

    แก้ไขสามารถเปิด portได้ละ

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
+// app.module.ts
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '',
+    //   database: 'fullstack_db',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+  ],
+  //controllers: [AppService],
 })
 export class AppModule {}
