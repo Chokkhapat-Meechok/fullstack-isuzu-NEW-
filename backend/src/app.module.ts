46f2a0102b9bb03d4b634f320e5e2946fc0fb8c6
Chokkhapat-Meechok <6731503006@lamduan.mfu.ac.th>
2025-10-16 12:31:51 +0000

    จัด project structure และแก้ไข Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true, // ให้มันสร้างตารางให้อัตโนมัติ
     }),
+
+    UserModule, //เพิ่มโมดูล users เข้ามาในระบบ
   ],
 })
 export class AppModule {}
